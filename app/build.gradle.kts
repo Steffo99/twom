fea2c46a48415a304906fa795ff7bbc1dbb6313e
Stefano Pigozzi <me@steffo.eu>
2023-11-04 14:57:29 +0100

    Bump targetSdk to 34

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "eu.steffo.twom"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "eu.steffo.twom"
         minSdk = 29
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
