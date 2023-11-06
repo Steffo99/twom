dc926e881428c68609d8aded78c798e644c96faf
Stefano Pigozzi <me@steffo.eu>
2023-11-06 02:36:14 +0100

    Increase `minSdk` to `31` (Android 12)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "eu.steffo.twom"
-        minSdk = 29
+        minSdk = 31
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
