6c6eaaf58edb8f3274ca06048c2ff173d87c0f7c
Stefano Pigozzi <me@steffo.eu>
2024-01-17 12:14:50 +0100

    Allow customization of `style` in `ErrorText` and `LoadingText`

--- app/src/main/java/eu/steffo/twom/composables/errorhandling/ErrorText.kt
+++ app/src/main/java/eu/steffo/twom/composables/errorhandling/ErrorText.kt
@@ -1,10 +1,12 @@
 package eu.steffo.twom.composables.errorhandling
 
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import eu.steffo.twom.R
 
@@ -14,10 +16,12 @@ import eu.steffo.twom.R
 fun ErrorText(
     modifier: Modifier = Modifier,
     text: String? = null,
+    style: TextStyle = LocalTextStyle.current
 ) {
     Text(
         modifier = modifier,
         text = text ?: stringResource(R.string.error),
         color = MaterialTheme.colorScheme.error,
+        style = style,
     )
 }
