8ca7b58a9269cfa44953b603e025dfa9cd737a9c
Stefano Pigozzi <me@steffo.eu>
2024-01-17 12:12:33 +0100

    `ErrorText`: default to `R.string.error` if no `text` is specified

--- app/src/main/java/eu/steffo/twom/composables/errorhandling/ErrorText.kt
+++ app/src/main/java/eu/steffo/twom/composables/errorhandling/ErrorText.kt
@@ -4,16 +4,20 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import eu.steffo.twom.R
 
 
 @Composable
+@Preview
 fun ErrorText(
-    text: String,
     modifier: Modifier = Modifier,
+    text: String? = null,
 ) {
     Text(
         modifier = modifier,
-        text = text,
+        text = text ?: stringResource(R.string.error),
         color = MaterialTheme.colorScheme.error,
     )
 }
